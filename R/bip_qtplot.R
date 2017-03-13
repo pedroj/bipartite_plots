65df81fbe35bc33ebbb134a8cb9b162ae1a16c6d
Pedro Jordano <jordano@ebd.csic.es>
2017-03-14 00:09:09 +0100

    Fixed edges in bip_qtplot.

--- R/bip_qtplot.R
+++ R/bip_qtplot.R
@@ -1,29 +1,31 @@
 # --------------------------------------------------------------------------
 # Plotting a bipartite network from the weighted (quantitative) adjacency
-# matrix of a two-mode network. Uses objects of type network.
+# matrix of a two-mode network. Uses objects of type network and a 
+# traditional plot.
 #
 bip_qtplot <- function (mat) {
     # Energy, weighted graphs
     ewt <- vectorize(mat)  
-    net <- network.initialize(dim(mat)[1] + dim(mat)[2],
-                              bipartite = c(dim(mat)[1]), 
-                              directed=F)
-    net <- network.bipartite(as.matrix(mat), net)
+    net <- network(mat,
+                   matrix.type = "bipartite",
+                   ignore.eval = FALSE,
+                   # names.eval = "weights")
+                   names.eval=list(row.names(mat), colnames(mat)))
 # This scales the weighted links for a better graphing
     ewt.scaled <- log(ewt[,3] + 1.0) / max(log(ewt[,3] + 1.0)) / 2
     plot.network(net,
                  usearrows=FALSE, jitter= T,
                  #   mode= "circle",
-                 #   mode= "fruchtermanreingold",
-                 mode = "kamadakawai", 
-                 label = network.vertex.names(mat1), displaylabels = T,
+                 mode= "fruchtermanreingold",
+                 #mode = "kamadakawai", 
+                 label = network.vertex.names(net), displaylabels = T,
                  boxed.labels = F,
                  label.pad =0,
                  label.pos = 5,
-                 label.cex = 0.5,
-                 vertex.col = c(rep("brown2",dim(mat)[1]),
-                              rep("darkolivegreen3",dim(mat)[2])), 
-                 vertex.cex = 2,
+                 label.cex = 0.7,
+                 vertex.col = c(rep("#FC9272",dim(mat)[1]),
+                              rep("#9ECAE1",dim(mat)[2])), 
+                 vertex.cex = 3,
                  vertex.sides = c(rep(5,dim(mat)[1]),
                                  rep(20,dim(mat)[2])),
                  vertex.lty = 1, 
@@ -31,7 +33,7 @@ bip_qtplot <- function (mat) {
                  # edge.lwd=(ewt[,3])/6,
                  edge.lwd = 25 * (ewt.scaled), # Adjust constant for 
                                                # better line widths
-                 edge.col = "grey", edge.curve = 0.2,
-                 label.lty = NULL, usecurve = F)
+                 edge.col = "#D9D9D9", edge.curve = 0.025, 
+                 label.lty = NULL, usecurve = T)
 }
 
