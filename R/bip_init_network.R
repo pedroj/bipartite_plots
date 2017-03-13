9e870c0a35cc2b12625956200b232d4ad8adccc4
Pedro Jordano <jordano@ebd.csic.es>
2017-03-13 23:11:05 +0100

    New code for initializing a bipartite network.

--- R/bip_init_network.R
+++ R/bip_init_network.R
@@ -1,14 +1,14 @@
 # Function to initialize a bipartite web for library network (type network),
 # starting from an adjacency matrix.
-bip_init_network <- function (mymat, modes = c("A", "P")) {
+bip_init_network <- function (mymat, modes= c("A", "P")) {
     require(network)
+    require(ggnet)
     if(!is.matrix(mymat)) mymat <- as.matrix(mymat)
-    a = dim(mymat)[1]
-    p = dim(mymat)[2]
-    net <- network.initialize(a + p,
-                               bipartite = a, directed=FALSE)
-    network.bipartite(mymat, net, 
-                      names.eval=list(row.names(mymat), colnames(mymat)))
-    set.vertex.attribute(net,"mode",
-                         c(rep(modes[1], a), rep(modes[2], p)))
+    a<- dim(mymat)[1]    # Animals are rows
+    p<- dim(mymat)[2]    # Plants are columns
+    net<- network(mymat,
+                  matrix.type = "bipartite",
+                  ignore.eval = FALSE,
+    #             names.eval = "weights")
+                  names.eval=list(row.names(mymat), colnames(mymat)))
 }
