749d24aa769245ad8d5ab34135aa13c96494a75d
Pedro Jordano <jordano@ebd.csic.es>
2017-03-14 20:00:54 +0100

    Added label option.

--- R/bip_railway.R
+++ R/bip_railway.R
@@ -1,7 +1,7 @@
 # Plot layout coordinates for railway networkplot
 # A matrix whose rows contain the x,y coordinates of the vertices of d.
 #
-bip_railway <- function (mymat, nodesize=9) {
+bip_railway <- function (mymat, nodesize=9, label=F) {
         # Coords for mode "A"
         coordA<- cbind(rep(2,dim(mymat)[1]), seq(1, dim(mymat)[1])+2)
         # Coords for mode "P"
@@ -10,7 +10,7 @@ bip_railway <- function (mymat, nodesize=9) {
 #
 # Initialize and plot the network with a railway layout.
         test.net<- bip_init_network(mymat)
-        p<- ggnet2(test.net, mode=mylayout, label=T,
+        p<- ggnet2(test.net, mode=mylayout, label=label,
                    size= nodesize, label.size=nodesize/3,
                    layout.exp=1.5) + coord_flip()
         p
