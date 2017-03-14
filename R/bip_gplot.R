3ed741ef94900fdd5cc8f8d403f08ba9e3941817
Pedro Jordano <jordano@ebd.csic.es>
2017-03-14 20:29:32 +0100

    Revised code for bip_gplot a and bip_railway.

--- R/bip_gplot.R
+++ R/bip_gplot.R
@@ -10,21 +10,22 @@ bip_gplot <- function (mymat, g) {
         stop("bip_gplot requires a graph object.")
     igraph.options(label.dist= 0, label.degree= pi/2, label.cex= 1,
                    label.family= "mono",
+#                  label.color= "white",
                    edge.lty= 1, 
                    curved= 0.5,
-                   edge.color= rgb(0, 0, 0, 0.5),
+                   edge.color= rgb(0, 0, 0, 0.3),
                    verbose=TRUE)
     plot(g,
          layout= layout.kamada.kawai,
          # vertex.color=c(rep(c("coral3",alpha=0.6), dim(mymat)[1]),
          #          rep(c("darkolivegreen3",alpha=0.6), dim(mymat)[2])),
-         vertex.color=c(rep(rgb(0.8, 0.1, 0, 0.8), dim(mymat)[1]),
-                        rep(rgb(0, 0.8, 0.2, 0.8), dim(mymat)[2])),
+         vertex.color=c(rep(rgb(0.9, 0.1, 0, 0.9), dim(mymat)[1]),
+                        rep(rgb(0, 0.9, 0.2, 0.9), dim(mymat)[2])),
          #        mark.col= c("coral3","darkolivegreen3"),
          vertex.size= 12,
          vertex.shape= c(rep("square",dim(mymat)[1]),
                          rep("circle",dim(mymat)[2])),
-         edge.width=25*(ewt.scaled)) # Adjust constant for 
-    # better line widths
-    #        edge.width=E(g)$weight/20)
+         edge.width=15*(ewt.scaled)) # Adjust constant for 
+                                     # better line widths
+                                     # edge.width=E(g)$weight/20)
 }
