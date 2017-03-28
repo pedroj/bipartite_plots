54ce2e697fb65bfd85a5613e26dcec8823e729d4
Pedro Jordano <jordano@ebd.csic.es>
2017-03-28 13:44:00 +0200

    Fixed errors in !is.network call. Knitted.

--- R/bip_ggnet.R
+++ R/bip_ggnet.R
@@ -32,7 +32,7 @@ bip_ggnet<- function(net, mat, mode= "fruchtermanreingold", size= 9,
                      layout.exp= 0)
 {
     source("./R/bip_edgewt.R")
-    if(!is.network(M)) stop("Must first initialize the network; use 'bip_init_network.R'.")
+    if(!is.network(net)) stop("Must first initialize the network; use 'bip_init_network.R'.")
     #
     # Set colors for each mode to setup a palette.
     col= c("A"= "grey", "P"= "gold")
--- R/bip_init_igraph.R
+++ R/bip_init_igraph.R
@@ -1,9 +1,9 @@
 # Function to initialize a bipartite web for library 
 # igraph (type igraph), starting from a weighted adjacency matrix.
 # Create the graph object (a bipartite, weighted graph) for igraph.
-bip_init_igraph <- function (mymat) {
+bip_init_igraph <- function (mat) {
                 require(igraph)
-                graph_from_incidence_matrix(mymat, 
-                        weighted= T, add.names= NULL, bipartite=dim(mymat)[1])
+                graph_from_incidence_matrix(mat, 
+                        weighted= T, add.names= NULL)
 }
  
