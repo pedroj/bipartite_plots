68f4014675de14ec2aabf03c66f4a0c8246b9985
Pedro Jordano <jordano@ebd.csic.es>
2018-04-08 17:18:09 +0200

    Added man code for function bip_init_igraph.R.

--- R/bip_init_igraph.R
+++ R/bip_init_igraph.R
@@ -1,9 +1,23 @@
-# Function to initialize a bipartite web for library 
+#' Function to initialize a bipartite web for library igraph (type igraph).
+#'
+#' @import igraph
+#' @param mat the adjacency matrix. Input can be a data.frame or a matrix, representing the adjacency matrix.
+#'
+#' @details Function to initialize a bipartite web for library igraph (type igraph), starting from a weighted adjacency matrix.
+#' Create the graph object (a bipartite, weighted graph) for igraph.
+#'
+#' @return A graph object.
+#'
+#' @examples
+#' #------------------------------------------------------------------------
+#' #### NOT RUN # Working on it.
+#' #------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# Function to initialize a bipartite web for library
 # igraph (type igraph), starting from a weighted adjacency matrix.
 # Create the graph object (a bipartite, weighted graph) for igraph.
 bip_init_igraph <- function (mat) {
                 require(igraph)
-                graph_from_incidence_matrix(mat, 
+                graph_from_incidence_matrix(mat,
                         weighted= T, add.names= NULL)
 }
- 
