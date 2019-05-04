2b7388e93ac6e3208a5d9799f0ae4e3000963901
Pedro Jordano <jordano@ebd.csic.es>
2019-05-04 19:44:28 +0200

    Fixed encodings.

--- R/bip_binplot.R
+++ R/bip_binplot.R
@@ -1,6 +1,6 @@
-#' Function bip_binplot, to plot a network object corresponding 
+#' Function bip_binplot, to plot a network object corresponding
 #' to a binary adjacency matrix.
-#' 
+#'
 #' mymat is the adjacency matrix, to get the dimensions from.
 #' net is a network object corresponding to mymat. The object net can
 #' be initialized with function bip_netinit.R
@@ -19,8 +19,8 @@
 #' # Working on it.
 #' #------------------------------------------------------------------------
 #----------------------------------------------------------------------------
-bip_binplot <- function (mymat, net, 
-                         usearrows=FALSE, 
+bip_binplot <- function (mymat, net,
+                         usearrows=FALSE,
                          mode= "kamadakawai",
                          displaylabels=   T,
                          label.cex=     0.6,
@@ -28,19 +28,19 @@ bip_binplot <- function (mymat, net,
                          edge.lty=      0.7
                          ) {
     #   require(grDevices)
-    if (!is.network(net)) 
+    if (!is.network(net))
         stop("plot.network requires a network object.")
-    if (network.size(net) == 0) 
-        stop("plot.network called on a network of order zero - 
+    if (network.size(net) == 0)
+        stop("plot.network called on a network of order zero -
              nothing to plot.")
     plot.network(net,
                  usearrows=usearrows, jitter= T,
                  #   mode= "circle",
                  #   mode= "fruchtermanreingold",
-                 mode= "kamadakawai", 
-                 label=network.vertex.names(net), 
+                 mode= "kamadakawai",
+                 label=network.vertex.names(net),
                  displaylabels = displaylabels,
-                 boxed.labels= F, 
+                 boxed.labels= F,
                  label.pad=0, label.pos= 5, label.cex= label.cex,
                  #  vertex.col=c(rep(rgb(1, 0, 0, 0.6),dim(mymat)[1]),
                  #               rep(rgb(0, 1, 0, 0.6),dim(mymat)[2])),
@@ -49,6 +49,6 @@ bip_binplot <- function (mymat, net,
                  vertex.cex= vertex.cex,
                  vertex.sides= c(rep(5,dim(mymat)[1]),
                                  rep(20,dim(mymat)[2])),
-                 vertex.lty= 0, edge.lty= edge.lty, edge.col= 8, 
+                 vertex.lty= 0, edge.lty= edge.lty, edge.col= 8,
                  label.lty= NULL, usecurve = F)
 }
